9b13cd061a5af25e371426db18c4273f0ba00828
dbsalsgur <rkawkaos123@naver.com>
2024-05-10 12:44:03 +0900

    test

--- HRMProject/src/main/java/employee/service/RegisterService.java
+++ HRMProject/src/main/java/employee/service/RegisterService.java
@@ -19,7 +19,7 @@ public class RegisterService {
 	private EmployeeEplyDao employeeEplyDao = new EmployeeEplyDao();
 
 	// JoinRequest객체로 정보를 받아,
-	public void Register(JoinRequest joinReq) {
+	public void Register(InfoRequest joinReq) {
 		Connection conn = null;
 		try {
 			// db와의 소통을 위해 connection 생성
