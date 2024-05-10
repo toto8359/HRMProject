d3e6306e13f0680b98cdf14480467cb208bc9f8e
user <user@DESKTOP-6QFQHVJ>
2024-05-10 14:40:31 +0900

    fd

--- HRMProject/src/main/java/jdbc/connection/ConnectionProvider.java
+++ HRMProject/src/main/java/jdbc/connection/ConnectionProvider.java
@@ -4,13 +4,17 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-//데이터베이스 연결을 제공하는 클래스
+// 데이터베이스 연결을 제공하는 클래스
+// データベース接続を提供するクラス
 public class ConnectionProvider {
 
-	// 데이터베이스 연결을 얻는 메소드
-	public static Connection getConnection() throws SQLException {
-		// Apache Commons DBCP를 사용하여 데이터베이스 연결 풀에서 연결을 가져옴
-		return DriverManager.getConnection("jdbc:apache:commons:dbcp:HRMProject");
-	}
+  // 데이터베이스 연결을 얻는 메소드
+  // データベース接続を取得するメソッド
+
+  public static Connection getConnection() throws SQLException {
+    // Apache Commons DBCP를 사용하여 데이터베이스 연결 풀에서 연결을 가져옴
+    // Apache Commons DBCPを使用してデータベース接続プールから接続を取得します
+    return DriverManager.getConnection("jdbc:apache:commons:dbcp:HRMProject");
+  }
 
 }
