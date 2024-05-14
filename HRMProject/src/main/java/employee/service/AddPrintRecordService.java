2cbce5c73bc4ec6981fffd190daf2e5c6f9b3b63
whtjdgml3624 <167948262+whtjdgml3624@users.noreply.github.com>
2024-05-15 02:39:15 +0900

    Update AddPrintRecordService.java
    쥬석

--- HRMProject/src/main/java/employee/service/AddPrintRecordService.java
+++ HRMProject/src/main/java/employee/service/AddPrintRecordService.java
@@ -11,6 +11,7 @@ public class AddPrintRecordService {
 	VOEPrintRecordDao voePrintRecordDao = new VOEPrintRecordDao();
 	
 	//재직증명서 발급시, 재직증명서 발급대장에 기록 추가
+	//在職証明書の発行時、在職証明書の発行台帳に記録を追加
 	public void insertVOEPrintRecord(String employeeNum) {
 		try (Connection conn = ConnectionProvider.getConnection()) {
 
