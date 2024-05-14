a8783e81f8c2ba1e714c713de932f7ca11327853
whtjdgml3624 <167948262+whtjdgml3624@users.noreply.github.com>
2024-05-14 17:51:14 +0900

    Update EmployeeListPageAll.java
    ㄶ

--- HRMProject/src/main/java/employee/service/EmployeeListPageAll.java
+++ HRMProject/src/main/java/employee/service/EmployeeListPageAll.java
@@ -3,9 +3,11 @@ package employee.service;
 import java.util.List;
 
 //사원정보 List 페이징을 위한 VO
+// 社員情報リストのページングのためのVO
 public class EmployeeListPageAll {
 
 	// 속성
+	// 属性
 	private int total;
 	private int currentPage;
 	private List<InfoRequestAll> content;
@@ -14,27 +16,33 @@ public class EmployeeListPageAll {
 	private int endPage;
 
 	// 생성자, 저장된 데이터 갯수에 따라 나머지 속성 결정
+	// コンストラクタ、保存されたデータ数に応じて残りの属性を決定
 	public EmployeeListPageAll(int total, int currentPage, int size, List<InfoRequestAll> content) {
 		this.total = total;
 		this.currentPage = currentPage;
 		this.content = content;
 
 		// 저장된 사원정보가 없으면
+		// 保存された社員情報がない場合
 		if (total == 0) {
 			totalPages = 0;
 			startPage = 0;
 			endPage = 0;
 		} else {
 			// 저장된 사원정보가 있으면
+			 // 保存された社員情報がある場合
 			totalPages = total / size;
 
 			// 총 사원정보 수를, 한 페이지당 표시 수로 나눠서 나머지가 있으면,
 			// 나머지 표시하기 위해 페이지수 +1
+			// 総社員情報数を、1ページ当たりの表示数で割って余りがある場合、
+            		// 余りを表示するためにページ数を+1
 			if (total % size > 0) {
 				totalPages++;
 			}
 
 			// 페이지 목록 표시를 위함
+			// ページリスト表示のため
 			int modVal = currentPage % 5;
 			startPage = currentPage / 5 * 5 + 1;
 			if (modVal == 0)
@@ -47,6 +55,7 @@ public class EmployeeListPageAll {
 	}
 
 	//view에서 사원정보 존재 확인
+	// ビューで社員情報の存在確認
 	public boolean hasNoEmployeeList() {
 		return total == 0;
 	}
@@ -56,6 +65,7 @@ public class EmployeeListPageAll {
 	}
 
 	// 게터 세터
+	// ゲッター・セッター
 	public int getTotal() {
 		return total;
 	}
