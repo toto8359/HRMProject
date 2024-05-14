775caa0dda7113f4fe3f69006cc512d0c3d82e6b
whtjdgml3624 <167948262+whtjdgml3624@users.noreply.github.com>
2024-05-15 02:30:18 +0900

    Update PrintVOEHandler.java
    쥬석

--- HRMProject/src/main/java/employee/command/PrintVOEHandler.java
+++ HRMProject/src/main/java/employee/command/PrintVOEHandler.java
@@ -34,12 +34,17 @@ public class PrintVOEHandler implements CommandHandler{
 	}
 
 	// get으로 받으면
+	// GETで受け取る場合
 	private String processForm(HttpServletRequest req, HttpServletResponse res) {
 		// get으로 받을 경우:
 		// 1.사원정보 List 띄우기
 		// 2.List의 이름 누르면 해당사원 정보 보여주기
+		// GETで受け取る場合:
+   		// 1.社員情報のリストを表示する
+    		// 2.リストの名前をクリックすると、その社員の情報を表示する
 
 		// 1.사원정보List띄우기-------------------------------------------------------------------------------------------------
+		// 1.社員情報リストを表示する-------------------------------------------------------------------------------------------------
 		String pageNoVal = req.getParameter("pageNo");
 		int pageNo = 1;
 		if (pageNoVal != null) {
@@ -48,8 +53,12 @@ public class PrintVOEHandler implements CommandHandler{
 		EmployeeListPagePart employeeListPagePart = listEmployeeInfoService.getEmployeeListPagePart(pageNo);
 		req.setAttribute("employeeListPagePart", employeeListPagePart);
 		// 1.사원정보List띄우기-------------------------------------------------------------------------------------------------
-
+		// 1.社員情報リストを表示する-------------------------------------------------------------------------------------------------
+   
+		
 		// 2.이름 누르면 정보 보여주기------------------------------------------------------------------------------------------
+		// 2.名前をクリックすると情報を表示する------------------------------------------------------------------------------------------
+    
 		String employeeNum = req.getParameter("employeeNum");
 		
 		SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
@@ -61,17 +70,23 @@ public class PrintVOEHandler implements CommandHandler{
 			req.setAttribute("readInfo", Boolean.TRUE);
 		}
 		// 2.이름 누르면 정보 보여주기------------------------------------------------------------------------------------------
-		
+		// 2.名前をクリックすると情報を表示する------------------------------------------------------------------------------------------
+    
 		return FORM_VIEW;
 	}
 
 	// post로 받으면
+	// POSTで受け取る場合
 	private String processSubmit(HttpServletRequest req, HttpServletResponse res) {
 		// post으로 받을 경우
 		// 1.사원정보 List 띄우기
 		// 2.인쇄버튼 누르면 인쇄대장 기록추가
+		// POSTで受け取る場合
+    		// 1.社員情報のリストを表示する
+    		// 2.印刷ボタンをクリックすると印刷台帳に記録を追加する
 		
 		// 1.사원정보List띄우기-------------------------------------------------------------------------------------------------
+		// 1.社員情報リストを表示する-------------------------------------------------------------------------------------------------
 		String pageNoVal = req.getParameter("pageNo");
 		int pageNo = 1;
 		if (pageNoVal != null) {
@@ -80,7 +95,8 @@ public class PrintVOEHandler implements CommandHandler{
 		EmployeeListPagePart employeeListPagePart = listEmployeeInfoService.getEmployeeListPagePart(pageNo);
 		req.setAttribute("employeeListPagePart", employeeListPagePart);
 		// 1.사원정보List띄우기-------------------------------------------------------------------------------------------------
-		
+		// 1.社員情報リストを表示する-------------------------------------------------------------------------------------------------
+    
 		
 		// 2.인쇄버튼 누르면 인쇄대장 기록추가----------------------------------------------------------------------------------
 		String employeeNumPrint = req.getParameter("employeeNumPrint");
@@ -88,8 +104,10 @@ public class PrintVOEHandler implements CommandHandler{
 			addPrintRecordService.insertVOEPrintRecord(employeeNumPrint);
 		}
 		// 2.인쇄버튼 누르면 인쇄대장 기록추가----------------------------------------------------------------------------------
-
+		// 2.印刷ボタンをクリックすると印刷台帳に記録を追加する----------------------------------------------------------------------------------
+    
 		// 마지막 돌아갈 페이지
+		// 最後に戻るページ
 		return FORM_VIEW;
 	}
 }
